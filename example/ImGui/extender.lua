4ddf63be6385e5b7d3b925e4ce3e013c3998d318
GENTA7740 <68140834+GENTA7740@users.noreply.github.com>
2025-07-23 22:53:11 +0700

    Base imgui.

--- example/ImGui/extender.lua
+++ example/ImGui/extender.lua
@@ -1,1 +1,16 @@
+local checkboxes = { false, false, false, false }
 
+function imDemo(deltaTime)
+    ImGui.Begin("GSCRIPT")
+
+    for i = 1, 4 do
+        local changed, value = ImGui.Checkbox("Checkbox " .. i, checkboxes[i])
+        if changed then
+            checkboxes[i] = value
+        end
+    end
+
+    ImGui.End()
+end
+
+AddHook("OnRender", "LABEL_01", imDemo)
