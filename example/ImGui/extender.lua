c7bd17db8bd4a83d3e85d9c1981151515db9662f
GENTA7740 <68140834+GENTA7740@users.noreply.github.com>
2025-07-24 16:05:48 +0700

    Update extender.lua

--- example/ImGui/extender.lua
+++ example/ImGui/extender.lua
@@ -14,14 +14,10 @@ local g_StateName = {
     "Skip update",
 }
 
-
-
 local g_TabNames = {
     "\xef\x80\x93",
     "\xef\x82\x85",
-};
-
-
+}
 
 local g_AutoEnum = {
     PUT_AND_BREAK = 1,
@@ -36,6 +32,45 @@ local g_AutoName = {
 local g_States = { false, false, false, false, false }
 local g_CurrentMainTab = 1
 local g_CurrentAutoTab = 1
+local g_AutoCollectState = false
+-- COLLECT CLASS, I'll clean it later --
+AutoCollect = {}
+AutoCollect.__index = AutoCollect
+
+function AutoCollect:new()
+    local self = setmetatable({}, AutoCollect)
+    self.is_active = false
+    self.range = 3
+    self.delay = 180
+    self.lastTime = 0
+    return self
+end
+
+function AutoCollect:currentTimeMillis()
+    return getCurrentTimeInternal()
+end
+
+function AutoCollect:shouldRun()
+    if not self.is_active then return false end
+    local now = self:currentTimeMillis()
+    if now - self.lastTime >= self.delay then
+        self.lastTime = now
+        return true
+    end
+    return false
+end
+
+function AutoCollect:run()
+    if self:shouldRun() then
+        runThread(function()
+            requestCollectByRange(self.range)
+        end)
+    end
+end
+
+-- END
+
+g_Collect = AutoCollect:new()
 
 function fHookOnRaw(raw)
     local packetHandlers = {
@@ -59,8 +94,8 @@ function fHookOnRaw(raw)
             logToConsole("Skipping update...")
         end,
         [25] = function()
-            logToConsole("[BAN BYPASS] Auto ban packet have been blocked!")
-            return true;
+            logToConsole("[`"..math.random(1, 9).."BAN BYPASS``] Auto ban packet have been blocked!")
+            return true
         end,
     }
 
@@ -106,44 +141,78 @@ function fCheatsMenu(deltaTime)
 end
 
 function fAutomationsMenu(deltaTime)
-    ImGui.BeginChild("_AUTO_1", ImVec2(ImGui.GetContentRegionAvailWidth() * 0.222, 0))
-    for i = 1, #g_AutoName do
-        if ImGui.ButtonWithStripe(g_AutoName[i], (g_CurrentAutoTab == i), ImGui.GetContentRegionAvailWidth()) then
-            g_CurrentAutoTab = i
+
+    if ImGui.BeginChild("MY_AUTO_1", ImVec2(ImGui.GetContentRegionAvailWidth() * 0.222, 0)) then
+        for i = 1, #g_AutoName do
+            if ImGui.ButtonWithStripe(g_AutoName[i], (g_CurrentAutoTab == i), ImGui.GetContentRegionAvailWidth()) then
+                g_CurrentAutoTab = i
+            end
         end
     end
     ImGui.EndChild()
     ImGui.SameLine()
-    ImGui.BeginChild("_AUTO_2", ImVec2(ImGui.GetContentRegionAvailWidth(), 0), true)
+    if ImGui.BeginChild("MY_AUTO_2", ImVec2(ImGui.GetContentRegionAvailWidth(), 0), true) then
+        local menuCases = {
+                [1] = function()
+                end,
+                [2] = function()
+                    local changed, value = ImGui.Checkbox("Toggle", g_AutoCollectState)
+                    if changed then
+                        g_AutoCollectState = value
+                        g_Collect.is_active = g_AutoCollectState;
+                    end
 
+                    local range = g_Collect.range
+                    local delay = g_Collect.delay
+
+                    local changed1, newRange = ImGui.SliderInt("Range", range, 1, 10)
+                    if changed1 then g_Collect.range = newRange end
+
+                    local changed2, newDelay = ImGui.SliderInt("Delay", delay, 180, 1000)
+                    if changed2 then g_Collect.delay = newDelay end
+                end,
+        }
+
+        local handler = menuCases[g_CurrentAutoTab]
+        if handler then
+            handler(deltaTime)
+        end
+    end
     ImGui.EndChild()
 end
 
 -- END MENU --
 
-function fRenderHook(deltaTime)
-    ImGui.Begin("GENTAHAX - Extender")
-    ImGui.BeginChild("_MAIN_CHILD_01", ImVec2(ImGui.GetContentRegionAvailWidth() / 10.0, 0), false)
-    for i = 1, #g_TabNames do
-        if ImGui.ButtonWithStripe(g_TabNames[i], (g_CurrentMainTab == i), ImGui.GetContentRegionAvailWidth()) then
-            g_CurrentMainTab = i
-        end
-    end
-    
-    ImGui.EndChild();
-    ImGui.SameLine();
-    ImGui.BeginChild("_MAIN_CHILD_02", ImVec2(ImGui.GetContentRegionAvailWidth(), 0));
-    local menuCases = {
-        [1] = fCheatsMenu,
-        [2] = fAutomationsMenu,
-    }
+function fEventLoop()
+    g_Collect:run()
+end
 
-    local handler = menuCases[g_CurrentMainTab]
-    if handler then
-        handler(deltaTime)
+function fRenderHook(deltaTime)
+    if ImGui.Begin("GENTAHAX - Extender") then
+        if ImGui.BeginChild("MY_MAIN_CHILD_01", ImVec2(ImGui.GetContentRegionAvailWidth() / 10.0, 0), false) then
+            for i = 1, #g_TabNames do
+                if ImGui.ButtonWithStripe(g_TabNames[i], (g_CurrentMainTab == i), ImGui.GetContentRegionAvailWidth()) then
+                    g_CurrentMainTab = i
+                end
+            end
+        end
+        ImGui.EndChild()
+        ImGui.SameLine()
+        if ImGui.BeginChild("MY_MAIN_CHILD_02", ImVec2(ImGui.GetContentRegionAvailWidth(), 0)) then
+            local menuCases = {
+                [1] = fCheatsMenu,
+                [2] = fAutomationsMenu,
+            }
+
+            local handler = menuCases[g_CurrentMainTab]
+            if handler then
+                handler(deltaTime)
+            end
+        end
+        ImGui.EndChild()
     end
-    ImGui.EndChild();
     ImGui.End()
+    fEventLoop()
 end
 
 AddHook("OnRender", "LABEL_01", fRenderHook)
