78b720a32a2c02d9dbdf405fc97ba85b6251cd86
GENTA7740 <Genta22barca@gmail.com>
2026-06-19 02:27:32 +0700

    Revert "Update extender.lua"
    This reverts commit 2dd3fc6bbf0324081bf4bacb662d71efffd5c633.

--- example/ImGui/extender.lua
+++ example/ImGui/extender.lua
@@ -20,13 +20,11 @@ local g_StateName = {
 local g_EspEnum = {
     NPC_ESP = 1,
     PLAYER_ESP = 2,
-    SEED_READY_ESP = 3,
 }
 
 local g_EspName = {
     "NPC ESP",
-    "Player ESP",
-    "Tree Ready ESP"
+    "Player ESP"
 }
 
 local g_TabNames = {
@@ -54,7 +52,8 @@ local g_CustomName = ""
 -- COLLECT CLASS, I'll clean it later --
 AutoCollect = {}
 AutoCollect.__index = AutoCollect
-
+local RED_U32 = ImGui.ColorConvertFloat4ToU32(ImVec4(1.0, 0.0, 0.0, 1.0))
+local WHITE_U32 = ImGui.ColorConvertFloat4ToU32(ImVec4(1.0, 1.0, 1.0, 0.0))
 function AutoCollect:new()
     local self = setmetatable({}, AutoCollect)
     self.is_active = false
@@ -80,9 +79,7 @@ end
 
 function AutoCollect:run()
     if self:shouldRun() then
-        runThread(function()
-            requestCollectByRange(self.range)
-        end)
+        requestCollectByRange(self.range)
     end
 end
 
@@ -227,14 +224,17 @@ end
 function fNpcEsp()
     if g_EspStates[g_EspEnum.NPC_ESP] then
         local p_local = getLocal()
-        if p_local ~= nil then
+        if type(p_local) == "table" then
             local _p = worldToScreen(p_local.pos.x, p_local.pos.y);
-            for _, v in pairs(getNpc()) do
-                if v ~= nil then
-                    local _start = worldToScreen(v.current.x - 16, v.current.y - 16);
-                    local _end = worldToScreen(v.current.x + 16, v.current.y + 16);
-                    ImGui.BG:AddRect(_start, _end, 0xFF2CFF00, 0.0, 1.0);
-                    ImGui.BG:AddLine(_p, _start, 0xFF2CFF00, 1.0);
+            local npc_table = getNpc()
+            if type(npc_table) == "table" then
+                for _, v in pairs(npc_table) do
+                    if v and v.current and v.current.x and v.current.y then
+                        local _start = worldToScreen(v.current.x - 16, v.current.y - 16)
+                        local _end = worldToScreen(v.current.x + 16, v.current.y + 16)
+                        ImGui.BG:AddRect(_start, _end, 0xFF2CFF00, 0.0, 1.0)
+                        ImGui.BG:AddLine(_p, _start, 0xFF2CFF00, 1.0)
+                    end
                 end
             end
         end
@@ -243,34 +243,16 @@ end
 function fPlayerEsp()
     if g_EspStates[g_EspEnum.PLAYER_ESP] then
         local p_local = getLocal()
-        if p_local ~= nil then
+        if type(p_local) == "table" then
             local _p = worldToScreen(p_local.pos.x, p_local.pos.y);
-            for _, v in pairs(getPlayerlist()) do
-                if v ~= nil then
-                    local _start = worldToScreen(v.pos.x - 8, v.pos.y - 8);
-                    local _end = worldToScreen((v.pos.x + 20 ) + 5, (v.pos.y + 30) + 5);
-                    ImGui.BG:AddRect(_start, _end, 0xFFFFFFFF, 0.0, 1.0);
-                    ImGui.BG:AddLine(_p, _start, 0xFFFFFFFF, 1.0);
-                end
-            end
-        end
-    end
-end
-function fSeedReadyEsp()
-    if g_EspStates[g_EspEnum.SEED_READY_ESP] then
-        local p_local = getLocal()
-        if p_local ~= nil then
-            local _p = worldToScreen(p_local.pos.x, p_local.pos.y);
-            for _, v in pairs(getTile()) do
-                if v ~= nil then
-                    local p_extra = getExtraTile(v.pos.x, v.pos.y);
-                    if p_extra ~= nil then
-                        if p_extra.ready then
-                            local _start = worldToScreen((v.pos.x * 32) + 32,(v.pos.y * 32) + 32);
-                            local _end = worldToScreen((v.pos.x * 32), (v.pos.y * 32));
-                            ImGui.BG:AddRect(_start, _end, 0xFF2CFF00, 0.0, 1.0);
-                            ImGui.BG:AddLine(_p, _start, 0xFF2CFF00, 1.0);
-                        end
+            local players_table = getPlayerlist()
+            if type(players_table) == "table" then
+                for _, v in pairs(players_table) do
+                    if v ~= nil then
+                        local _start = worldToScreen(v.pos.x - 8, v.pos.y - 8);
+                        local _end = worldToScreen((v.pos.x + 20 ) + 5, (v.pos.y + 30) + 5);
+                        ImGui.BG:AddRect(_start, _end, 0xFFFFFFFF, 0.0, 1.0);
+                        ImGui.BG:AddLine(_p, _start, 0xFFFFFFFF, 1.0);
                     end
                 end
             end
@@ -281,7 +263,6 @@ function fEventLoop()
     g_Collect:run()
     fNpcEsp()
     fPlayerEsp()
-    fSeedReadyEsp()
 end
 
 function fRenderHook(deltaTime)
@@ -292,7 +273,7 @@ function fRenderHook(deltaTime)
                 
                 ImGui.PushStyleColor(
                     ImGui.Col.Button,
-                    (g_CurrentMainTab == i) and ImGui.ColorConvertFloat4ToU32(ImVec4(1.0, 0.0, 0.0, 1.0)) or ImGui.ColorConvertFloat4ToU32(ImVec4(1.0, 1.0, 1.0, 0.0))
+                    (g_CurrentMainTab == i) and RED_U32 or WHITE_U32
                 )
                 if ImGui.ButtonWithStripe(g_TabNames[i], (g_CurrentMainTab == i), ImGui.GetContentRegionAvailWidth()) then
                     g_CurrentMainTab = i
@@ -317,14 +298,25 @@ function fRenderHook(deltaTime)
     end
     ImGui.End()
     fEventLoop()
+    collectgarbage("collect")
+	collectgarbage("setstepmul", 30)
 end
 function fOnVarlist(vlist, netid)
-    if vlist[0] == "OnSDBroadcast" and g_States[g_StateEnum.BLOCK_SDB] then
-        logToConsole("[`4ANTI SDB``] Super duper broadcast have been blocked!")
-        return true
-    end
-    if vlist[0] == "OnConsoleMessage" and vlist[1]:match("CP:0_PL:4_OID:_CT:%[SB%]_") and g_States[g_StateEnum.BLOCK_SB] then
-        return true
+    local varCase = {
+        [hash32("OnSDBroadcast")] = function()
+            logToConsole("[`4ANTI SDB``] Super duper broadcast have been blocked!")
+            return true
+        end,
+        [hash32("OnConsoleMessage")] = function()
+            if vlist[1]:match("CP:0_PL:4_OID:_CT:%[SB%]_") and g_States[g_StateEnum.BLOCK_SB] then
+                return true
+            end
+        end,
+    }
+
+    local handler = varCase[hash32(vlist[0])]
+    if handler then
+        return handler() or false
     end
   return false
 end
@@ -335,6 +327,87 @@ function fOntextPacket(type, pkt)
     if warpTarget then
         sendPacket(3, "action|join_request\nname|"..warpTarget:upper().."\ninvitedWorld|0\n")
         logToConsole("`"..math.random(1, 9).."Warping to``: `w"..warpTarget:upper().."``")
+        return true
+    end
+    local bscan_det = pkt:lower():match("/bscan")
+    if bscan_det then
+        local _dmp = ""
+        local _nmap = {}
+
+        for _, tile in pairs(getTile()) do
+            if tile then
+                if tile.fg and tile.fg ~= 0 then
+                    if _nmap[tile.fg] == nil then
+                        _nmap[tile.fg] = 1
+                    else
+                        _nmap[tile.fg] = _nmap[tile.fg] + 1
+                    end
+                end
+
+                if tile.bg and tile.bg ~= 0 then
+                    if _nmap[tile.bg] == nil then
+                        _nmap[tile.bg] = 1
+                    else
+                        _nmap[tile.bg] = _nmap[tile.bg] + 1
+                    end
+                end
+            end
+        end
+
+        for itemID, count in pairs(_nmap) do
+            _dmp = _dmp .. tostring(itemID) .. "," .. tostring(count) .. ","
+        end
+
+        local dlgd =
+            "add_label_with_icon|big|World Blocks.|left|6016|\n" ..
+            "add_spacer|small\n" ..
+            "add_label_with_icon_button_list|small|`w%s : %s|left|blockScan_|itemID_itemAmount|" ..
+            _dmp ..
+            "\nadd_spacer|small|\n" ..
+            "end_dialog|gscan_block|Exit||\n" ..
+            "add_quick_exit|\n"
+
+        local var = {}
+        var[0] = "OnDialogRequest"
+        var[1] = dlgd
+        sendVariant(var, -1, 100)
+
+
+        return true
+    end
+    local fscan_det = pkt:lower():match("/fscan")
+    if fscan_det then
+        local _dump = ""
+        local mapped = {}
+
+        for _, obj in pairs(getWorldObject()) do
+            if obj and obj.id then
+                if mapped[obj.id] == nil then
+                    mapped[obj.id] = obj.amount
+                else
+                    mapped[obj.id] = mapped[obj.id] + obj.amount
+                end
+            end
+        end
+
+        for itemID, amount in pairs(mapped) do
+            _dump = _dump .. tostring(itemID) .. "," .. tostring(amount) .. ","
+        end
+
+        local dlgd =
+            "add_label_with_icon|big|Floating objects.|left|6016|\n" ..
+            "add_spacer|small\n" ..
+            "add_label_with_icon_button_list|small|`w%s : %s|left|floatingScan_|itemID_itemAmount|" ..
+            _dump ..
+            "\nadd_spacer|small|\n" ..
+            "end_dialog|gscan_float|Exit||\n" ..
+            "add_quick_exit|\n"
+
+        local var = {}
+        var[0] = "OnDialogRequest"
+        var[1] = dlgd
+        sendVariant(var, -1, 100)
+
         return true
     end
     return false
